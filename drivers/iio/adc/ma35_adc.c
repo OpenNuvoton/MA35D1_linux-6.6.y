9fb45d3a6a52e2467f337a66c6f204e82328fac4
yclu4 <yclu4@nuvoton.com>
2026-05-26 15:45:31 +0800

    fix: correct clock resume sequence in adc driver add: add pm support to eadc driver
    Change-Id: Ice57392e99f3a468d11a911ae31c7397b3389b21

--- drivers/iio/adc/ma35_adc.c
+++ drivers/iio/adc/ma35_adc.c
@@ -416,14 +416,14 @@ static int ma35_adc_resume(struct device *dev)
 	struct iio_dev *indio_dev = dev_get_drvdata(dev);
 	struct ma35_iio_priv *priv = iio_priv(indio_dev);
 
+	clk_prepare_enable(priv->adc_clk);
+
 	/* enable irq */
 	enable_irq(priv->irq);
 
 	/* Power up the ADC */
 	__raw_writel(__raw_readl(priv->base + REG_ADC_CTL) | ADC_CTL_ADEN,
 		priv->base + REG_ADC_CTL);
-
-	clk_prepare_enable(priv->adc_clk);
 
 	return 0;
 }
